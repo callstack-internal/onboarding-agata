02b28e562e4d38a46095f49fb95bfb316079e78f
Agata Kosior <agata.kosior57@gmail.com>
2023-02-08 14:51:47 +0100

    fix: remove console.log

--- src/modules/notifications/index.ts
+++ src/modules/notifications/index.ts
@@ -3,7 +3,6 @@ import {NativeModules} from 'react-native';
 class NotificationModuleInterface {
   static showAppNotification(title: string, body: string) {
     const module = NativeModules.MyNotification;
-    console.log(module);
 
     module.showNotification(title, body);
   }

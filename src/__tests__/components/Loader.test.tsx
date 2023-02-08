a44d18da9e40d74b0922b8a1f4ca43de591f205a
Agata Kosior <agata.kosior57@gmail.com>
2023-02-08 10:58:38 +0100

    fix: add assertion for loader

--- src/__tests__/components/Loader.test.tsx
+++ src/__tests__/components/Loader.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, screen} from '@testing-library/react-native';
 
 import {Loader} from '../../components/Loader';
 
 describe('Loader component', () => {
   it('should render correctly', () => {
     render(<Loader />);
+
+    expect(screen.getByTestId('loader')).toBeOnTheScreen();
   });
 });

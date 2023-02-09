dbafede4c769583aacd8d047cb2cdea9c47761d2
Agata Kosior <agata.kosior57@gmail.com>
2023-02-09 11:11:40 +0100

    fix: change multiple tests into one scenario

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -7,24 +7,18 @@ describe('Weather app', () => {
     await device.reloadReactNative();
   });
 
-  it('should display a list of cities', async () => {
+  it('should display a list of cities, navigate to the details page when pressing one of the list items and navigate back to the main page when pressing Weather button or back button', async () => {
     await expect(element(by.id('list'))).toBeVisible();
-  });
 
-  it('should navigate to the details page when pressing one of the list items', async () => {
-    const firstElement = element(by.id('listItem-0'));
-    await expect(firstElement).toBeVisible();
-    await element(by.id('listItem-1')).tap();
-    await expect(firstElement).not.toBeVisible();
-  });
-
-  it('should navigate back to the main page when pressing Weather button or back button', async () => {
     const firstElement = element(by.id('listItem-0'));
 
     await expect(firstElement).toBeVisible();
-    await element(by.id('listItem-1')).tap();
+
+    await firstElement.tap();
 
     await expect(firstElement).not.toBeVisible();
+    await expect(element(by.text('Pressure:'))).toBeVisible();
+    await expect(element(by.text('Humidity:'))).toBeVisible();
 
     if (device.getPlatform() === 'android') {
       await device.pressBack();
@@ -33,33 +27,5 @@ describe('Weather app', () => {
     }
 
     await expect(firstElement).toBeVisible();
-  });
-
-  it('should show city name, icon, weather conditions and temperature for each city both on main and details page', async () => {
-    await expect(element(by.id('listItem-0-name'))).toBeVisible();
-    await expect(element(by.id('listItem-0-temperature'))).toBeVisible();
-    await expect(element(by.id('listItem-0-conditions'))).toBeVisible();
-    await expect(element(by.id('listItem-0-icon'))).toBeVisible();
-
-    await element(by.id('listItem-0')).tap();
-
-    await expect(element(by.id('details-name'))).toBeVisible();
-    await expect(element(by.id('details-temperature'))).toBeVisible();
-    await expect(element(by.id('details-conditions'))).toBeVisible();
-    await expect(element(by.id('details-icon'))).toBeVisible();
-  });
-
-  it('should show extra weather data on the details page', async () => {
-    await expect(element(by.text('Pressure:'))).not.toBeVisible();
-    await expect(element(by.text('Humidity:'))).not.toBeVisible();
-    await expect(element(by.text('Wind Speed:'))).not.toBeVisible();
-    await expect(element(by.text('Cloud Cover:'))).not.toBeVisible();
-
-    await element(by.id('listItem-0')).tap();
-
-    await expect(element(by.text('Pressure:'))).toBeVisible();
-    await expect(element(by.text('Humidity:'))).toBeVisible();
-    await expect(element(by.text('Wind Speed:'))).toBeVisible();
-    await expect(element(by.text('Cloud Cover:'))).toBeVisible();
   });
 });
